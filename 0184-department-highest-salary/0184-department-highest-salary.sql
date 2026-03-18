38aa1a20cb9a6d9ed3d915e1242662c3fb11f34f
Bijaya kumar jena <158011164+Bijaya-423@users.noreply.github.com>
2026-03-18 12:58:30 +0530

    Time: 1179 ms (6.91%), Space: 0B (100%) - LeetHub

---
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -0,0 +1,10 @@
+/* Write your PL/SQL query statement below */
+select d.name as Department , e.name as Employee, e.salary as Salary
+from Employee e, Department d
+where e.departmentId = d.id
+and (e.departmentId, e.salary) in (select departmentId, max(salary)
+from Employee
+group by departmentId)
+
+
+
