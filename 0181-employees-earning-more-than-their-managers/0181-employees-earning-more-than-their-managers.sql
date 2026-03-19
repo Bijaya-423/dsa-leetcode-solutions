9f30cf688a0de8ff0db58ea776b90853951a5c5a
Bijaya kumar jena <158011164+Bijaya-423@users.noreply.github.com>
2026-03-19 12:11:38 +0530

    Time: 405 ms (83.56%), Space: 0B (100%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -1,5 +1,12 @@
 /* Write your PL/SQL query statement below */
-select e.name as Employee
-from Employee e , Employee m
+
+/*select e.name as Employee
+from Employee e, Employee m
 where e.managerId = m.id
-and e.salary > m.salary
+and e.salary > m.salary;*/
+
+select e.name "Employee"
+from Employee e
+join Employee m
+on e.managerId = m.id
+where e.salary > m.salary;
