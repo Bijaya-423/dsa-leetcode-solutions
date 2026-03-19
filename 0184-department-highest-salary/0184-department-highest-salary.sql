60133346cf8aa2965a81c1586374b1ef88e25f6b
Bijaya kumar jena <158011164+Bijaya-423@users.noreply.github.com>
2026-03-19 16:53:04 +0530

    Time: 749 ms (30.89%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,8 +1,5 @@
 /* Write your PL/SQL query statement below */
-select d.name as Department,
- e.name as Employee,
- e.salary as Salary
+select d.name as Department, e.name as Employee, e.salary as Salary
 from Employee e, Department d
 where e.departmentId = d.id
-and (e.departmentId, e.salary) in
- (select departmentId, max(salary) from Employee group by departmentId )
+and (e.departmentId, e.salary) in (select departmentId, max(salary) from Employee group by departmentId)
