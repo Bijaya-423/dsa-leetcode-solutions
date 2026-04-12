db50432771822c1373f46fef91dbe08c67b5d660
Bijaya kumar jena <158011164+Bijaya-423@users.noreply.github.com>
2026-04-12 22:21:22 +0530

    Time: 77 ms (78.22%), Space: 32.7 MB (86.7%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.py
+++ 0221-maximal-square/0221-maximal-square.py
@@ -12,7 +12,7 @@ class Solution:
             for j in range(cols):
                 if matrix[i][j] == "1":
                     if i == 0 or j == 0:
-                        dp[i][j] = 1
+                        dp[i][j] = 1 
                     else:
                         dp[i][j] = 1+min(
                             dp[i-1][j],
