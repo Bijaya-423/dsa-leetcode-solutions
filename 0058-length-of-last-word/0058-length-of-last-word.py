d8ab936a50fae606e926492a81d834fd0067e6e1
Bijaya kumar jena <158011164+Bijaya-423@users.noreply.github.com>
2026-05-13 23:00:15 +0530

    Time: 0 ms (100%), Space: 19.3 MB (15.66%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -2,7 +2,7 @@ class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         # remove the extra spaces from start and end
         s.strip()
-
+        # Converts string into list
         words = s.split()
 
         return len(words[-1])
